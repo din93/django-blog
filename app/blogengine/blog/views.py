c6f87f6d1ec5b2a99c91d2bf1bbe0d9b33118116
RadikDinov <din93ya@yandex.ru>
2018-09-30 02:03:31 +0500

    Добавлен суперпользователь для блога. Установлено ограничение для CRUD. Настроен постоянный редирект на страницу блога с корневого адреса

--- app/blogengine/blog/views.py
+++ app/blogengine/blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.views.generic import View
 from django.urls import reverse
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 from .models import Post, Tag
 from .utils import *
@@ -14,19 +15,22 @@ class PostDetail(ObjectDetailMixin, View):
     model = Post
     template = 'blog/post_detail.html'
 
-class PostCreate(ObjectCreateMixin, View):
+class PostCreate(LoginRequiredMixin, ObjectCreateMixin, View):
     form_model = PostForm
     template = 'blog/post_create.html'
+    raise_exception = True
 
-class PostUpdate(ObjectUpdateMixin, View):
+class PostUpdate(LoginRequiredMixin, ObjectUpdateMixin, View):
     model = Post
     form_model = PostForm
     template = 'blog/post_update.html'
+    raise_exception = True
 
-class PostDelete(ObjectDeleteMixin, View):
+class PostDelete(LoginRequiredMixin, ObjectDeleteMixin, View):
     model = Post
     template = 'blog/post_delete.html'
     redirect_url = 'posts_list_url'
+    raise_exception = True
 
 def tags_list(request):
     tags = Tag.objects.all()
@@ -36,16 +40,19 @@ class TagDetail(ObjectDetailMixin, View):
     model = Tag
     template = 'blog/tag_detail.html'
 
-class TagCreate(ObjectCreateMixin, View):
+class TagCreate(LoginRequiredMixin, ObjectCreateMixin, View):
     form_model = TagForm
     template = 'blog/tag_create.html'
+    raise_exception = True
 
-class TagUpdate(ObjectUpdateMixin, View):
+class TagUpdate(LoginRequiredMixin, ObjectUpdateMixin, View):
     model = Tag
     form_model = TagForm
     template = 'blog/tag_update.html'
+    raise_exception = True
 
-class TagDelete(ObjectDeleteMixin, View):
+class TagDelete(LoginRequiredMixin, ObjectDeleteMixin, View):
     model = Tag
     template = 'blog/tag_delete.html'
     redirect_url = 'tags_list_url'
+    raise_exception = True
