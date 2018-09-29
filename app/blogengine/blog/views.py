a6aee1e80af8726fc3a39c1611ff253947ca28e5
RadikDinov <din93ya@yandex.ru>
2018-09-29 17:53:27 +0500

    Добавлены формы удаления для постов и тэгов

--- app/blogengine/blog/views.py
+++ app/blogengine/blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.views.generic import View
+from django.urls import reverse
 
 from .models import Post, Tag
 from .utils import *
@@ -22,6 +23,11 @@ class PostUpdate(ObjectUpdateMixin, View):
     form_model = PostForm
     template = 'blog/post_update.html'
 
+class PostDelete(ObjectDeleteMixin, View):
+    model = Post
+    template = 'blog/post_delete.html'
+    redirect_url = 'posts_list_url'
+
 def tags_list(request):
     tags = Tag.objects.all()
     return render(request, 'blog/tags_list.html', context={'tags': tags})    
@@ -38,3 +44,8 @@ class TagUpdate(ObjectUpdateMixin, View):
     model = Tag
     form_model = TagForm
     template = 'blog/tag_update.html'
+
+class TagDelete(ObjectDeleteMixin, View):
+    model = Tag
+    template = 'blog/tag_delete.html'
+    redirect_url = 'tags_list_url'
